f28b7457e62f4e5701b06ce087cb768c3d808251
jor <joo0u0oor@gmail.com>
2022-06-10 04:02:51 +0800

    FINISHED annotating create/join-domain.ps1, install-utilities.ps1

--- Vagrant/scripts/install-utilities.ps1
+++ Vagrant/scripts/install-utilities.ps1
@@ -1,23 +1,35 @@
 # Purpose: Installs chocolatey package manager, then installs custom utilities from Choco.
 
+# if there is no existing test path C:\ProgramData\chocolatey
 If (-not (Test-Path "C:\ProgramData\chocolatey")) {
+  # set security protocol type to tls12
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Chocolatey"
+  # download chocolatey install contents from the website, runs the install as a string
   Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 } else {
+  # chocolatey is already installed
   Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Chocolatey is already installed."
 }
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing utilities..."
+
+# downloads custom windows 7 start menu in windows 10----- NOT NEEDED -------------------------------------------
 If ($(hostname) -eq "win10") {
   # Because the Windows10 start menu sucks
   choco install -y --limit-output --no-progress classic-shell -installArgs ADDLOCAL=ClassicStartMenu
   & "C:\Program Files\Classic Shell\ClassicStartMenu.exe" "-xml" "c:\vagrant\resources\windows\MenuSettings.xml"
   regedit /s c:\vagrant\resources\windows\MenuStyle_Default_Win7.reg
 }
-choco install -y --limit-output --no-progress NotepadPlusPlus WinRar processhacker
+# ---------------------------------------------------------------------------------------------------------------
 
+# downloads processhacker (better task manager)------------- NOT VERY USEFUL ------------------------------------
+choco install -y --limit-output --no-progress NotepadPlusPlus WinRar processhacker
+# ---------------------------------------------------------------------------------------------------------------
+
+# downloads google chrome----------------------------- QUITE USEFUL ---------------------------------------------
 # This repo often causes failures due to incorrect checksums, so we ignore them for Chrome
 choco install -y --limit-output --no-progress --ignore-checksums GoogleChrome 
+# ---------------------------------------------------------------------------------------------------------------
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Utilties installation complete!"
--- Vagrant/scripts/join-domain.ps1
+++ Vagrant/scripts/join-domain.ps1
@@ -1,25 +1,34 @@
 # Purpose: Joins a Windows host to the windomain.local domain which was created with "create-domain.ps1".
 # Source: https://github.com/StefanScherer/adfs2
 
+# a variable that is never used :o
 $hostsFile = "c:\Windows\System32\drivers\etc\hosts"
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Joining the domain..."
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) First, set DNS to DC to join the domain..."
+# new dns server (dc's ip address)
 $newDNSServers = "192.168.56.102"
+# get network adapters with the ip address containing "192.168.36."
 $adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPAddress -match "192.168.56."}
 # Don't do this in Azure. If the network adatper description contains "Hyper-V", this won't apply changes.
 # Specify the DC as a WINS server to help with connectivity as well
+# if the network adapter's description does not contain "hyper-v", set new dns to dc's ip address
 $adapters | ForEach-Object {if (!($_.Description).Contains("Hyper-V")) {$_.SetDNSServerSearchOrder($newDNSServers); $_.SetWINSServer($newDNSServers, "")}}
 
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Now join the domain..."
+# get the host name of the machine
 $hostname = $(hostname)
+# path locating the user
 $user = "windomain.local\vagrant"
+# password for user
 $pass = ConvertTo-SecureString "vagrant" -AsPlainText -Force
+# set user and password into a new PSCredential object
 $DomainCred = New-Object System.Management.Automation.PSCredential $user, $pass
 
 # Place the computer in the correct OU based on hostname
 # Retry up to 3 times. Sleep 15 seconds between tries.
+# if the hostname is "wef" or "exchange"---------------------------------- NOT NEEDED -------------------------------------------------------------------------------------------
 If (($hostname -eq "wef") -or ($hostname -eq "exchange")) {
   $tries = 0
   While ($tries -lt 3) {
@@ -37,21 +46,30 @@ If (($hostname -eq "wef") -or ($hostname -eq "exchange")) {
   Set-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control' -Name 'WaitToKillServiceTimeout' -Value '500' -Type String -Force -ea SilentlyContinue
   New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Desktop' -Name 'AutoEndTasks' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue
   Set-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\SessionManager\Power' -Name 'HiberbootEnabled' -Value 0 -Type DWord -Force -ea SilentlyContinue
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# WINDOWS 10 joins the domain
 } ElseIf ($hostname -eq "win10") {
   Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Adding Win10 to the domain. Sometimes this step times out. If that happens, just run 'vagrant reload win10 --provision'" #debug
+  # adds windows 10 to "windomain.local" and distinguished ou name (found in properties) with credentials
   Add-Computer -DomainName "windomain.local" -credential $DomainCred -OUPath "ou=Workstations,dc=windomain,dc=local"
 } Else {
+  # if fail, just add windows 10 to the domain without ou
   Add-Computer -DomainName "windomain.local" -credential $DomainCred -PassThru
 }
 
 # Stop Windows Update
 Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Disabling Windows Updates and Windows Module Services"
+# disable windows update auto service
 Set-Service wuauserv -StartupType Disabled
+# stop windows update auto service
 Stop-Service wuauserv
+# disable trusted installer
 Set-Service TrustedInstaller -StartupType Disabled
+# stop trusted installer
 Stop-Service TrustedInstaller
 
-# Uninstall Windows Defender from WEF
+# Uninstall Windows Defender from WEF----------NOT NEEDED-------------------------------------------------------------------------------
 # This command isn't supported on WIN10
 If ($hostname -ne "win10" -And (Get-Service -Name WinDefend -ErrorAction SilentlyContinue).status -eq 'Running') {
   # Uninstalling Windows Defender (https://github.com/StefanScherer/packer-windows/issues/201)
@@ -64,3 +82,4 @@ If ($hostname -ne "win10" -And (Get-Service -Name WinDefend -ErrorAction Silentl
     Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) We'll try again during install-red-team.ps1"
   }
 }
+# --------------------------------------------------------------------------------------------------------------------------------------
